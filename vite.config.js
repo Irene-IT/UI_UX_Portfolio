e9d1b4e21981e71d899b12e97a99c3dcbd359bc6
Irene <irene.dev.sp@gmail.com>
2025-07-08 18:22:03 +0200

    the project is complete all files are perfect

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import path from "path"
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-// https://vite.dev/config/
+import tailwindcss from '@tailwindcss/vite'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
