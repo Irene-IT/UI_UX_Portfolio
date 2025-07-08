e5a201a8465a89c3345174bf8157be3fc82cf128
Irene <irene.dev.sp@gmail.com>
2025-07-08 16:37:41 +0200

    Revert "fin project"
    This reverts commit 4fbaf455f59de511eda2c3321018d9300b5231ee.

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,11 @@
-// import path from "path"
-// import tailwindcss from "@tailwindcss/vite"
-// import react from "@vitejs/plugin-react"
-// import { defineConfig } from "vite"
- 
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// })
-
-
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import path from "path"
-import { fileURLToPath } from "url"
+import tailwindcss from "@tailwindcss/vite";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss(),],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
