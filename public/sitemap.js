658e8ef019ef902480b7a49b190891b992219f16
Irene-IT <irene.it.work@gmail.com>
2025-11-17 12:51:17 +0100

    upd SEO

---
+++ public/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'http://ui-ux-dp.netlify.app',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+  ]
+}
